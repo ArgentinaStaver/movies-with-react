bd9857cfaf196e78220d97cb20ec4452e881391d
DESKTOP-52MPL8G\Arjush <argentinastaver@gmail.com>
2017-06-07 22:22:09 +0300

    Refactored FilmList component

--- src/Components/FilmsList.js
+++ src/Components/FilmsList.js
@@ -1,28 +1,32 @@
 import React, { Component } from 'react';
 
 class FilmsList extends Component {
-  render() {
-    const { movies, filterText } = this.props;
-    const moviesList = movies
-      .filter(film => {
-          return film.title.toLowerCase().indexOf(filterText.toLowerCase()) >= 0
+    moviesProgram() {
+      const { movies, filterText } = this.props;
+      const moviesList = movies
+        .filter(film => {
+            return film.title.toLowerCase().indexOf(filterText.toLowerCase()) >= 0
+        })
+        .map((film, index)=> {
+          return (
+            <div key={index} className="film">
+              <h6 className="title">{film.title}</h6>
+              <p className="year">Year: {film.year} </p>
+            </div>
+          )
       })
-      .map((film, index)=> {
-        return (
-          <div key={index} className="film">
-            <h6 className="title">{film.title}</h6>
-            <p className="year">Year: {film.year} </p>
+      return moviesList;
+    }
+
+    render() {
+      return(
+        <div className="page">
+          <div className="films-list">
+            {this.moviesProgram()}
           </div>
-        )
-    })
-    return(
-      <div className="page">      
-        <div className="films-list">
-          {moviesList}
         </div>
-      </div>
-    );
-  }
+      );
+    }
 }
 
 export default FilmsList;
